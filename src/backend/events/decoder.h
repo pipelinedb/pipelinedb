c78475b48dda6b55699f8e46bafe57cfc373b5c8
Derek Nelson <=>
2013-12-11 08:24:25 -0800

    Adding events package

---
+++ src/backend/events/decoder.h
@@ -0,0 +1,9 @@
+/*-----------------------------------------------------------------------------
+ * decoder.h
+ *		Interface for decoding raw event data
+ *
+ * This package implements decoding functionality for raw events
+ *
+ *
+ * src/events/decoder.h
+ *-----------------------------------------------------------------------------
