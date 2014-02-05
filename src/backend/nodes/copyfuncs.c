9f33aeda5c659f8179c80f0d00074253288ab15c
Derek Nelson <=>
2014-02-05 00:43:12 -0800

    Removing redundant fields

--- src/backend/nodes/copyfuncs.c
+++ src/backend/nodes/copyfuncs.c
@@ -2562,9 +2562,6 @@ _copyQuery(const Query *from)
 	COPY_SCALAR_FIELD(has_to_save_cmd_id);
 #endif
 	COPY_SCALAR_FIELD(is_continuous);
-	COPY_SCALAR_FIELD(cq_batch_size);
-	COPY_SCALAR_FIELD(cq_batch_timeout_ms);
-	COPY_SCALAR_FIELD(cq_pause_ms);
 	COPY_SCALAR_FIELD(cq_activate_stmt);
 
 	return newnode;
