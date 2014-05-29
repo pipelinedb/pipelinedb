59807d66ef68f6ca75021e877205161a4fccae5d
Derek Nelson <=>
2014-05-29 15:44:56 -0700

    Adding stream buffer modules

---
+++ src/backend/events/streambuf.c
@@ -0,0 +1,11 @@
+/*-------------------------------------------------------------------------
+ *
+ * stream.c
+ *
+ *	  Interface for interacting with the stream buffer
+ *
+ * src/backend/events/streambuf.c
+ *
+ *-------------------------------------------------------------------------
+ */
+#include "postgres.h"
---
+++ src/include/events/streambuf.h
@@ -0,0 +1,14 @@
+/*-------------------------------------------------------------------------
+ *
+ * stream.h
+ *
+ *	  Interface for interacting with the stream buffer
+ *
+ * src/include/events/streambuf.h
+ *
+ *-------------------------------------------------------------------------
+ */
+#ifndef STREAMBUF_H
+#define STREAMBUF_H
+
+#endif
