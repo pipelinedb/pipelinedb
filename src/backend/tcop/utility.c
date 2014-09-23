fecf83a847d7c7d24309243c822746b876d8cc84
Derek Nelson <=>
2014-09-23 15:50:37 -0700

    Fix DEACTIVATE command completion tag

--- src/backend/tcop/utility.c
+++ src/backend/tcop/utility.c
@@ -2467,7 +2467,7 @@ CreateCommandTag(Node *parsetree)
 			tag = "ACTIVATE CONTINUOUS VIEW";
 			break;
 		case T_DeactivateContinuousViewStmt:
-			tag = "DEACTIVATE CONTINUOUS VIEW";
+			tag = "DEACTIVATE";
 			break;
 		case T_DumpStmt:
 			tag = "DUMP";
