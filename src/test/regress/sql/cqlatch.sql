09846ce0d7514a318ee46ce211f86128fb22efb3
jaypipeline <jay@pipelinedb.com>
2014-10-17 11:53:16 -0700

    Tests for #298

---
+++ src/test/regress/sql/cqlatch.sql
@@ -0,0 +1,13 @@
+set debug_sync_stream_insert = on;
+CREATE CONTINUOUS VIEW cqlatch AS SELECT id::integer FROM stream_latch;
+ACTIVATE CONTINUOUS VIEW cqlatch;
+-- Wait at least 10 seconds till the worker latch is actually blocoed
+SELECT pg_sleep_for('12 seconds');
+-- Query the activity table verify that the worker is waiting on the latch
+select query,state from pg_stat_activity;
+-- Insert something into the stream so that the latch is released
+INSERT INTO stream_latch (id) VALUES (4);
+INSERT INTO stream_latch (id) VALUES (5);
+-- Query the activity table verify that the worker latch is unblocked
+select query,state from pg_stat_activity;
+DEACTIVATE CONTINUOUS VIEW cqlatch;
