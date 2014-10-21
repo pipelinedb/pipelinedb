d8c355390212b711759901c94f479e24478679ad
Usman Masood <usmanm@fastmail.fm>
2014-10-20 19:39:24 -0700

    Fix non-deterministic failure in cqlatch test

--- src/test/regress/sql/cqlatch.sql
+++ src/test/regress/sql/cqlatch.sql
@@ -4,10 +4,10 @@ ACTIVATE CONTINUOUS VIEW cqlatch;
 -- Wait at least 10 seconds till the worker latch is actually blocoed
 SELECT pg_sleep_for('3 seconds');
 -- Query the activity table verify that the worker is waiting on the latch
-select query,state from pg_stat_activity;
+select query, state FROM pg_stat_activity WHERE NOT query='';
 -- Insert something into the stream so that the latch is released
 INSERT INTO stream_latch (id) VALUES (4);
 INSERT INTO stream_latch (id) VALUES (5);
 -- Query the activity table verify that the worker latch is unblocked
-select query,state from pg_stat_activity;
+select query, state FROM pg_stat_activity WHERE NOT query='';
 DEACTIVATE CONTINUOUS VIEW cqlatch;
