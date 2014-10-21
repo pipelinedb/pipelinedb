f302279a876802a2c0d8d6a26a361be64e43a95b
Usman Masood <usmanm@fastmail.fm>
2014-10-20 19:26:57 -0700

    Make cqlatch sleep for less

--- src/test/regress/sql/cqlatch.sql
+++ src/test/regress/sql/cqlatch.sql
@@ -2,7 +2,7 @@ set debug_sync_stream_insert = on;
 CREATE CONTINUOUS VIEW cqlatch AS SELECT id::integer FROM stream_latch;
 ACTIVATE CONTINUOUS VIEW cqlatch;
 -- Wait at least 10 seconds till the worker latch is actually blocoed
-SELECT pg_sleep_for('12 seconds');
+SELECT pg_sleep_for('3 seconds');
 -- Query the activity table verify that the worker is waiting on the latch
 select query,state from pg_stat_activity;
 -- Insert something into the stream so that the latch is released
