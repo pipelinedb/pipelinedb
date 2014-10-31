ffc17691c849912a393e4e666bb73bb0fab0d22a
jaypipeline <jay@pipelinedb.com>
2014-10-31 10:14:34 -0700

    Executor goes through (at least for the simplest query)

--- src/backend/optimizer/plan/setrefs.c
+++ src/backend/optimizer/plan/setrefs.c
@@ -580,6 +580,7 @@ set_plan_refs(PlannerInfo *root, Plan *plan, int rtoffset)
 		case T_TuplestoreScan:
 			break;
 		case T_NestLoop:
+		case T_StreamTable:
 		case T_MergeJoin:
 		case T_HashJoin:
 			set_join_references(root, (Join *) plan, rtoffset);
