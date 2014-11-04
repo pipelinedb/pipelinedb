d3db1cce055bf3095ed12f0764145aa67717ae9c
Usman Masood <usmanm@fastmail.fm>
2014-11-04 15:05:59 -0800

    add avg test

--- src/test/regress/sql/cqwindow.sql
+++ src/test/regress/sql/cqwindow.sql
@@ -1,4 +1,4 @@
-CREATE CONTINUOUS VIEW cqwindow0 AS SELECT key::text, SUM(x::int) OVER (PARTITION BY key ROWS BETWEEN 1 PRECEDING AND 1 FOLLOWING) FROM stream;
+CREATE CONTINUOUS VIEW cqwindow0 AS SELECT key::text, SUM(x::numeric) OVER (PARTITION BY key ROWS BETWEEN 1 PRECEDING AND 1 FOLLOWING) FROM stream;
 \d+ cqwindow0_pdb;
 \d+ cqwindow0;
 ACTIVATE cqwindow0;
@@ -12,3 +12,18 @@ INSERT INTO stream (key, x) VALUES ('a', 7), ('b', 8);
 DEACTIVATE;
 SELECT key, sum FROM cqwindow0_pdb ORDER BY _0, key;
 SELECT * FROM cqwindow0 ORDER BY key;
+
+CREATE CONTINUOUS VIEW cqwindow1 AS SELECT key::text, AVG(x::int) OVER (PARTITION BY key ROWS BETWEEN 2 PRECEDING AND CURRENT ROW) FROM stream;
+\d+ cqwindow1_pdb;
+\d+ cqwindow1;
+ACTIVATE cqwindow1;
+INSERT INTO stream (key, x) VALUES ('a', 1), ('b', 2), ('a', 3);
+SELECT pg_sleep(1);
+INSERT INTO stream (key, x) VALUES ('a', 4), ('b', 5), ('b', 6);
+SELECT pg_sleep(1);
+INSERT INTO stream (key, x) VALUES ('a', 7), ('b', 8), ('a', 9);
+SELECT pg_sleep(1);
+INSERT INTO stream (key, x) VALUES ('a', 10), ('b', 11), ('b', 12);
+DEACTIVATE;
+SELECT key, avg FROM cqwindow1_pdb ORDER BY _0, key;
+SELECT * FROM cqwindow1 ORDER BY key;
