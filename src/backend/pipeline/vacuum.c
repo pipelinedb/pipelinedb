b243439de109e5fcf8d3532262b36151ee1a31e7
Usman Masood <usmanm@fastmail.fm>
2014-11-12 13:48:35 -0800

    Add vacuum.c/h

---
+++ src/backend/pipeline/vacuum.c
@@ -0,0 +1,14 @@
+/*-------------------------------------------------------------------------
+ *
+ * vacuum.c
+ *
+ *   Support for vacuuming materialization relations for sliding window
+ *   continuous views.
+ *
+ * src/backend/pipeline/vacuum.c
+ *
+ *-------------------------------------------------------------------------
+ */
+#include "postgres.h"
+
+#include "pipeline/vacuum.h"
---
+++ src/include/pipeline/vacuum.h
@@ -0,0 +1,15 @@
+/*-------------------------------------------------------------------------
+ *
+ * vacuum.h
+ *
+ *   Support for vacuuming materialization relations for sliding window
+ *   continuous views.
+ *
+ * src/include/pipeline/vacuum.h
+ *
+ *-------------------------------------------------------------------------
+ */
+#ifndef VACUUM_H
+#define VACUUM_H
+
+#endif /* VACUUM_H */
