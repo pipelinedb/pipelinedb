544c3987c26f0b8670223f6f5e72df6db680e9b1
Usman Masood <usmanm@fastmail.fm>
2014-11-15 22:06:37 -0800

    fas

--- src/test/regress/sql/cqvacuum.sql
+++ src/test/regress/sql/cqvacuum.sql
@@ -1,6 +1,6 @@
 SET debug_sync_stream_insert = 'on';
 
-CREATE CONTINUOUS VIEW cqvacuum AS SELECT key::text, COUNT(*) FROM cqvacuum_stream WHERE arrival_timestamp > clock_timestamp() - interval '4 second' GROUP BY key;
+CREATE CONTINUOUS VIEW cqvacuum AS SELECT key::text, COUNT(*) FROM cqvacuum_stream WHERE arrival_timestamp > clock_timestamp() - interval '3 second' GROUP BY key;
 ACTIVATE cqvacuum;
 INSERT INTO cqvacuum_stream (key) VALUES ('a'), ('b'), ('c');
 INSERT INTO cqvacuum_stream (key) VALUES ('a'), ('b'), ('c');
@@ -10,7 +10,7 @@ INSERT INTO cqvacuum_stream (key) VALUES ('a'), ('b'), ('c');
 DEACTIVATE cqvacuum;
 SELECT * FROM cqvacuum ORDER BY key;
 SELECT key, count FROM cqvacuum_mrel0 ORDER BY key, count;
-SELECT pg_sleep(4);
+SELECT pg_sleep(3);
 SELECT * FROM cqvacuum ORDER BY key;
 SELECT key, count FROM cqvacuum_mrel0 ORDER BY key, count;
 ACTIVATE cqvacuum;
