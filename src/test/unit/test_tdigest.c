c5f91d874ea2b375d490effe7a3efe651868e357
Usman Masood <usmanm@fastmail.fm>
2015-02-04 17:50:58 -0800

    t-digest accuracy improvement

--- src/test/unit/test_tdigest.c
+++ src/test/unit/test_tdigest.c
@@ -8,6 +8,37 @@
 
 #define DEBUG 0
 
+static void
+print_centroid(Centroid *c)
+{
+	int id = c->id;
+	printf("Centroid: %f [%d]\n", c->mean, id);
+}
+
+static void
+print_centroids(AVLNode *node)
+{
+	AVLNodeIterator *it = AVLNodeIteratorCreate(node, NULL);
+	Centroid *c;
+
+	while ((c = AVLNodeNext(it)))
+		print_centroid(c);
+
+	AVLNodeIteratorDestroy(it);
+}
+
+static void
+print_inorder(AVLNode *node)
+{
+	printf("%*s==\n", node->depth, "");
+	if (node->left)
+		print_inorder(node->left);
+	printf("%*sCentroid: %f [%d] (%d)\n", node->depth, "", node->leaf->mean, node->leaf->id, node->size);
+	if (node->right)
+		print_inorder(node->right);
+	printf("%*s==\n", node->depth, "");
+}
+
 static int
 int_cmp(const void * a, const void * b)
 {
@@ -32,6 +63,8 @@ centroid_cmp(Centroid *a, Centroid *b)
 		return -1;
 	if (a->id > b->id)
 		return 1;
+	if (a->id < b->id)
+		return -1;
 	return 0;
 }
 
@@ -49,39 +82,12 @@ check_balance(AVLNode *node)
 	ck_assert_int_eq(node->left->size + node->right->size, node->size);
 	ck_assert_int_eq(Max(l, r) + 1, node->depth);
 	ck_assert_int_eq(centroid_cmp(node->leaf, AVLNodeFirst(node->right)), 0);
+
+
+
 	ck_assert_int_le(centroid_cmp(node->left->leaf, node->right->leaf), 0);
 	check_balance(node->left);
 	check_balance(node->right);
-}
-
-static void
-print_centroid(Centroid *c)
-{
-	printf("Centroid: %f [%d]\n", c->mean, c->id);
-}
-
-static void
-print_centroids(AVLNode *node)
-{
-	AVLNodeIterator *it = AVLNodeIteratorCreate(node, NULL);
-	int i;
-
-	for (i = 0; i < node->count; i++)
-		print_centroid(AVLNodeNext(it));
-
-	AVLNodeIteratorDestroy(it);
-}
-
-static void
-print_inorder(AVLNode *node)
-{
-	printf("%*s==\n", node->depth, "");
-	if (node->left)
-		print_inorder(node->left);
-	printf("%*sCentroid: %f [%d] (%d)\n", node->depth, "", node->leaf->mean, node->leaf->id, node->size);
-	if (node->right)
-		print_inorder(node->right);
-	printf("%*s==\n", node->depth, "");
 }
 
 START_TEST(test_tree_adds)
@@ -92,8 +98,8 @@ START_TEST(test_tree_adds)
 
 	ck_assert_ptr_eq(AVLNodeFloor(root, c), NULL);
 	ck_assert_ptr_eq(AVLNodeCeiling(root, c), NULL);
+	ck_assert_int_eq(AVLNodeHeadSize(root, NULL), 0);
 	ck_assert_int_eq(AVLNodeHeadCount(root, NULL), 0);
-	ck_assert_int_eq(AVLNodeHeadSum(root, NULL), 0);
 
 	c = CentroidCreate();
 	CentroidAddSingle(c, 1);
@@ -259,42 +265,42 @@ START_TEST(test_tree_remove_and_sums)
 
 	check_balance(root);
 
+	ck_assert_int_eq(0, AVLNodeHeadSize(root, NULL));
 	ck_assert_int_eq(0, AVLNodeHeadCount(root, NULL));
-	ck_assert_int_eq(0, AVLNodeHeadSum(root, NULL));
 
 	c = CentroidCreateWithId(0);
 	CentroidAddSingle(c, 0);
 
+	ck_assert_int_eq(0, AVLNodeHeadSize(root, c));
 	ck_assert_int_eq(0, AVLNodeHeadCount(root, c));
-	ck_assert_int_eq(0, AVLNodeHeadSum(root, c));
 
 	CentroidDestroy(c);
 	c = CentroidCreateWithId(0);
 	CentroidAddSingle(c, 1);
 
+	ck_assert_int_eq(2, AVLNodeHeadSize(root, c));
 	ck_assert_int_eq(2, AVLNodeHeadCount(root, c));
-	ck_assert_int_eq(2, AVLNodeHeadSum(root, c));
 
 	CentroidDestroy(c);
 	c = CentroidCreateWithId(0);
 	CentroidAddSingle(c, 2.1);
 
+	ck_assert_int_eq(5, AVLNodeHeadSize(root, c));
 	ck_assert_int_eq(5, AVLNodeHeadCount(root, c));
-	ck_assert_int_eq(5, AVLNodeHeadSum(root, c));
 
 	CentroidDestroy(c);
 	c = CentroidCreateWithId(0);
 	CentroidAddSingle(c, 2.7);
 
-	ck_assert_int_eq(6, AVLNodeHeadCount(root, c));
-	ck_assert_int_eq(7, AVLNodeHeadSum(root, c));
+	ck_assert_int_eq(6, AVLNodeHeadSize(root, c));
+	ck_assert_int_eq(7, AVLNodeHeadCount(root, c));
 
 	CentroidDestroy(c);
 	c = CentroidCreateWithId(0);
 	CentroidAddSingle(c, 200);
 
-	ck_assert_int_eq(101, AVLNodeHeadCount(root, c));
-	ck_assert_int_eq(102, AVLNodeHeadSum(root, c));
+	ck_assert_int_eq(101, AVLNodeHeadSize(root, c));
+	ck_assert_int_eq(102, AVLNodeHeadCount(root, c));
 
 	CentroidDestroy(c);
 }
@@ -511,7 +517,7 @@ START_TEST(test_tdigest_merge)
 	for (i = 0; i < 100; i++)
 	{
 		x = rand();
-		ck_assert(fabs(TDigestCDF(t0, x) - TDigestCDF(t3, x)) < 0.005);
+		ck_assert(fabs(TDigestCDF(t0, x) - TDigestCDF(t3, x)) < 0.01);
 	}
 }
 END_TEST
