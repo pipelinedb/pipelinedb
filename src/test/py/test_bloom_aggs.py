e5c367136cafbd2485b48e4a5bd2afdcd024fcdf
Usman Masood <usmanm@fastmail.fm>
2015-03-04 14:54:46 -0800

    Change default p and n for bloom filters

--- src/test/py/test_bloom_aggs.py
+++ src/test/py/test_bloom_aggs.py
@@ -34,10 +34,10 @@ def test_user_low_and_high_card(pipeline, clean_db):
     assert result[0] == 4
 
     result = pipeline.execute('SELECT bloom_cardinality(combine(bloom_agg)) FROM test_bloom_agg WHERE k in (2, 3)').first()
-    assert result[0] == 9999
+    assert result[0] == 10001
 
     result = pipeline.execute('SELECT bloom_cardinality(combine(bloom_agg)) FROM test_bloom_agg').first()
-    assert result[0] == 10003
+    assert result[0] == 10005
 
 
 def test_bloom_agg_hashing(pipeline, clean_db):
@@ -73,9 +73,9 @@ def test_bloom_agg_hashing(pipeline, clean_db):
 
     result = result[0]
 
-    assert result[0] == 9999
-    assert result[1] == 20003
-    assert result[2] == 10001
+    assert result[0] == 10001
+    assert result[1] == 19991
+    assert result[2] == 10002
 
 
 def test_bloom_intersection(pipeline, clean_db):
@@ -111,7 +111,7 @@ def test_bloom_intersection(pipeline, clean_db):
 
   result = result[0]
 
-  assert result[0] == 5012
+  assert result[0] == 5071
 
 def test_bloom_contains(pipeline, clean_db):
   """
