ae92d295a93717528c7d6da010eef1162464d87a
Derek Nelson <=>
2015-03-04 17:42:04 -0800

    Fix it again

--- src/backend/pipeline/cqanalyze.c
+++ src/backend/pipeline/cqanalyze.c
@@ -1872,7 +1872,8 @@ AnalyzeAndValidateContinuousSelectStmt(ParseState *pstate, SelectStmt **topselec
 				foreach(lc, context.streams)
 				{
 					RangeVar *rv = (RangeVar *) lfirst(lc);
-					if (pg_strcasecmp(rv->relname, strVal(alias)) == 0 || pg_strcasecmp(rv->alias->aliasname, strVal(alias)) == 0)
+					if (pg_strcasecmp(rv->relname, strVal(alias)) == 0 ||
+							(rv->alias && pg_strcasecmp(rv->alias->aliasname, strVal(alias)) == 0))
 					{
 						table = rv;
 						break;
