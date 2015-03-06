1fbd84e57b3b9bc5580a538ea137683c7ce88986
Usman Masood <usmanm@fastmail.fm>
2015-03-05 18:06:12 -0800

    gcs

--- src/backend/pipeline/gcs.c
+++ src/backend/pipeline/gcs.c
@@ -1,9 +1,9 @@
 /*-------------------------------------------------------------------------
  *
- * bloom.c
- *	  Bloom Filter implementation.
+ * gcs.c
+ *	  Golomb-coded Set implementation.
  *
- * src/backend/pipeline/bloom.c
+ * src/backend/pipeline/gcs.c
  *
  *-------------------------------------------------------------------------
  */
@@ -15,3 +15,63 @@
 
 #define DEFAULT_P 0.02
 #define DEFAULT_N (2 << 17)
+
+GolombCodedSet *
+GolombCodedSetCreateWithPAndN(float8 p, uint32_t n)
+{
+
+}
+
+GolombCodedSet *
+GolombCodedSetCreate(void)
+{
+
+}
+
+void
+GolombCodedSetDestroy(GolombCodedSet *gcs)
+{
+
+}
+
+GolombCodedSet *
+GolombCodedSetCopy(GolombCodedSet *gcs)
+{
+
+}
+
+void
+GolombCodedSetAdd(GolombCodedSet *gcs, void *key, Size size)
+{
+
+}
+
+bool
+GolombCodedSetContains(GolombCodedSet *gcs, void *key, Size size)
+{
+
+}
+
+GolombCodedSet *
+GolombCodedSetUnion(GolombCodedSet *result, GolombCodedSet *incoming)
+{
+
+}
+
+GolombCodedSet *
+GolombCodedSetIntersection(GolombCodedSet *result, GolombCodedSet *incoming)
+{
+
+}
+
+float8
+GolombCodedSetFillRatio(GolombCodedSet *gcs)
+{
+
+}
+
+Size
+GolombCodedSetSize(GolombCodedSet *gcs)
+{
+
+}
