071ca2d8fd00bc0d400a40488f4a73691da37d6a
Derek Nelson <=>
2015-03-23 14:50:12 -0700

    #778 support copying into streams

--- src/include/pipeline/stream.h
+++ src/include/pipeline/stream.h
@@ -51,5 +51,6 @@ extern void DropPreparedStreamInsert(const char *name);
 extern bool InsertTargetIsStream(InsertStmt *ins);
 extern int InsertIntoStreamPrepared(PreparedStreamInsertStmt *pstmt);
 extern int InsertIntoStream(InsertStmt *ins, List *values);
+extern uint64 CopyIntoStream(const char *stream, TupleDesc desc, HeapTuple *tuples, int ntuples);
 
 #endif
