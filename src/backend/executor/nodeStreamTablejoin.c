eeb6186ee4b5f0349c862f28e38685aa4d541c77
Derek Nelson <=>
2015-04-18 14:40:39 -0700

    #833 use indexes in stream-table joins when possible

--- src/backend/executor/nodeStreamTablejoin.c
+++ src/backend/executor/nodeStreamTablejoin.c
@@ -373,6 +373,7 @@ ExecStreamTableJoin(StreamTableJoinState *node)
 					if (TupIsNull(node->hj_FirstOuterTupleSlot))
 					{
 						node->hj_OuterNotEmpty = false;
+						ExecReScan((PlanState *) node);
 						return NULL;
 					}
 					else
