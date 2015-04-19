a8325376c6597447b7297169c3f913db5caa3d3f
Derek Nelson <=>
2015-04-18 20:22:13 -0700

    #838 log NOTICE for stream-table joins on unindexed table columns

--- src/backend/optimizer/plan/subselect.c
+++ src/backend/optimizer/plan/subselect.c
@@ -2353,6 +2353,7 @@ finalize_plan(PlannerInfo *root, Plan *plan, Bitmapset *valid_params,
 							  &context);
 			break;
 
+		case T_StreamTableJoin:
 		case T_HashJoin:
 			finalize_primnode((Node *) ((Join *) plan)->joinqual,
 							  &context);
