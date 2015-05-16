222f4d6c782c61d2554274931d6023d8328303f6
Derek Nelson <=>
2015-05-15 17:51:13 -0700

    Fix memory leaks

--- src/backend/pipeline/combiner.c
+++ src/backend/pipeline/combiner.c
@@ -521,6 +521,8 @@ finish:
 		HeapTuple tup = (HeapTuple) lfirst(lc);
 		tuplestore_puttuple(batch, tup);
 	}
+
+	hash_destroy(batchgroups->hashtab);
 }
 
 /*
@@ -577,6 +579,7 @@ sync_combine(CombineState *cstate, Tuplestorestate *results, TupleHashTable exis
 		ResetPerTupleExprContext(estate);
 	}
 
+	FreeExecutorState(estate);
 	CQMatViewClose(ri);
 	relation_close(rel, RowExclusiveLock);
 }
