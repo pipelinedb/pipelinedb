99bffe8e4b468830e4cf40033bf5d929886d9537
Usman Masood <usmanm@fastmail.fm>
2015-05-28 15:50:39 -0700

    fix tests

--- src/test/regress/sql/cont_view_namespace.sql
+++ src/test/regress/sql/cont_view_namespace.sql
@@ -31,7 +31,7 @@ CREATE CONTINUOUS VIEW test_cvn0 AS SELECT x::int FROM test_cvn_stream;
 CREATE SCHEMA test_cvn_schema0;
 CREATE CONTINUOUS VIEW test_cvn_schema0.test_cvn0 AS SELECT x::int, y::text FROM test_cvn_stream;
 
-SELECT name, "desc" FROM pipeline_stream WHERE name='test_cvn_stream';
+SELECT name, "desc" FROM pipeline_stream WHERE name='test_cvn_stream' ORDER BY "desc";
 
 INSERT INTO test_cvn_stream (x) VALUES (1);
 INSERT INTO test_cvn_schema0.test_cvn_stream (x, y) VALUES (2, 2), (3, 3);
