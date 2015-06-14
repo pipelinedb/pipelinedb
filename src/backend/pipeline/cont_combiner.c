4812c1abc39e7f79c023313b7016998113a37819
Derek Nelson <=>
2015-06-14 13:24:06 -0700

    Set debug_query_string to currently executing CV name

--- src/backend/pipeline/cont_combiner.c
+++ src/backend/pipeline/cont_combiner.c
@@ -904,6 +904,7 @@ ContinuousQueryCombinerMain(void)
 				if (!TupleBufferBatchReaderHasTuplesForCQId(reader, id))
 					goto next;
 
+				debug_query_string = NameStr(state->view->name);
 				MemoryContextSwitchTo(state->tmp_cxt);
 
 				TupleBufferBatchReaderSetCQId(reader, id);
@@ -970,6 +971,8 @@ next:
 				cq_stat_report(false);
 			else
 				cq_stat_send_purge(id, 0, CQ_STAT_COMBINER);
+
+			debug_query_string = NULL;
 		}
 
 		CommitTransactionCommand();
