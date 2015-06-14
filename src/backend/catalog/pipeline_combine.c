bb5d2cee2595451cd4d8c4ca1b90e3a776b6160e
Derek Nelson <=>
2015-06-13 22:54:23 -0700

    #636 support user-defined combine functions

--- src/backend/catalog/pipeline_combine.c
+++ src/backend/catalog/pipeline_combine.c
@@ -11,11 +11,62 @@
  */
 #include "postgres.h"
 
+#include "access/heapam.h"
 #include "access/htup.h"
+#include "access/xact.h"
 #include "access/htup_details.h"
+#include "catalog/dependency.h"
+#include "catalog/indexing.h"
+#include "catalog/namespace.h"
 #include "catalog/pg_aggregate.h"
+#include "catalog/pg_proc.h"
+#include "catalog/pg_type.h"
 #include "catalog/pipeline_combine_fn.h"
+#include "commands/defrem.h"
+#include "miscadmin.h"
+#include "utils/acl.h"
+#include "utils/lsyscache.h"
+#include "utils/rel.h"
 #include "utils/syscache.h"
+
+/*
+ * lookup_single_func
+ *
+ * Lookup a function, and throw an error if there is not precisely one result
+ */
+static HeapTuple
+lookup_single_func(List *name)
+{
+	/* Get list of possible candidates from namespace search */
+	FuncCandidateList clist = FuncnameGetCandidates(name, -1, NIL, false, false, false);
+	HeapTuple tup;
+
+	/* there shouldn't be any ambiguity in resolving a combine function */
+	if (!clist)
+		elog(ERROR, "no function named %s found", NameListToString(name));
+	else if (clist->next)
+		elog(ERROR, "found multiple functions with name %s", NameListToString(name));
+
+	tup = SearchSysCache1(PROCOID, ObjectIdGetDatum(clist->oid));
+	if (!HeapTupleIsValid(tup))
+		elog(ERROR, "cache lookup failed for function %u", clist->oid);
+
+	return tup;
+}
+
+static Oid
+get_single_func_oid(List *name)
+{
+	HeapTuple t = lookup_single_func(name);
+	Oid result;
+
+	Assert(HeapTupleIsValid(t));
+
+	result = HeapTupleGetOid(t);
+	ReleaseSysCache(t);
+
+	return result;
+}
 
 /*
  * GetCombineInfo
@@ -53,7 +104,7 @@ GetCombineInfo(Oid aggfnoid, Oid *combinefn, Oid *combineinfn, Oid *statetype)
 	*combineinfn = combform->combineinfn;
 
 	/* we only have a state type if we're actually storing state in tables */
-	if (combform->storestate)
+	if (combform->aggfinalfn)
 		*statetype = combform->transouttype;
 
 	ReleaseSysCache(combtup);
@@ -64,13 +115,187 @@ GetCombineInfo(Oid aggfnoid, Oid *combinefn, Oid *combineinfn, Oid *statetype)
  *
  * Given an aggregate function oid, determine the type of its transition state
  */
-Oid GetCombineStateType(Oid aggfnoid)
+Oid
+GetCombineStateType(Oid aggfnoid)
 {
 	Oid combinefn;
 	Oid combineinfn;
 	Oid result;
 
 	GetCombineInfo(aggfnoid, &combinefn, &combineinfn, &result);
+
+	return result;
+}
+
+/*
+ * DefineCombiner
+ *
+ * Define a combiner that can be used to combine aggregate transition states for continuous views
+ */
+Oid
+DefineCombiner(Oid aggoid, List *name, List *args, bool oldstyle, List *parameters,
+		const char *queryString)
+{
+	char *aggname;
+	Oid namespace;
+	AclResult	aclresult;
+	ListCell *pl;
+	List *combinename = NIL;
+	List *transoutname = NIL;
+	List *combineinname = NIL;
+	HeapTuple	aggtup;
+	HeapTuple combinetup;
+	Form_pg_aggregate agg;
+	Relation pipeline_combine;
+	bool nulls[Natts_pipeline_combine];
+	Datum values[Natts_pipeline_combine];
+	Oid finalfn = InvalidOid;
+	Oid transfn = InvalidOid;
+	Oid transoutfn = InvalidOid;
+	Oid combineinfn = InvalidOid;
+	Oid combinefn = InvalidOid;
+	Oid transouttype = InvalidOid;
+	Oid result = InvalidOid;
+	ObjectAddress parent;
+	ObjectAddress child;
+
+	/* Convert list of names to a name and namespace */
+	namespace = QualifiedNameGetCreationNamespace(name, &aggname);
+
+	/* Check we have creation rights in target namespace */
+	aclresult = pg_namespace_aclcheck(namespace, GetUserId(), ACL_CREATE);
+	if (namespace != ACLCHECK_OK)
+		aclcheck_error(aclresult, ACL_KIND_NAMESPACE,
+					   get_namespace_name(namespace));
+
+	foreach(pl, parameters)
+	{
+		DefElem *defel = (DefElem *) lfirst(pl);
+		if (pg_strcasecmp(defel->defname, "combinefunc") == 0)
+			combinename = defGetQualifiedName(defel);
+		if (pg_strcasecmp(defel->defname, "combineinfunc") == 0)
+			combineinname = defGetQualifiedName(defel);
+		if (pg_strcasecmp(defel->defname, "transoutfunc") == 0)
+			transoutname = defGetQualifiedName(defel);
+	}
+
+	CommandCounterIncrement();
+
+	aggtup = SearchSysCache1(AGGFNOID, ObjectIdGetDatum(aggoid));
+	if (!HeapTupleIsValid(aggtup))
+		elog(ERROR, "cache lookup failed for aggregate %u", aggoid);
+
+	agg = (Form_pg_aggregate) GETSTRUCT(aggtup);
+
+	/*
+	 * If the trans type is internal, input and output functions are
+	 * required to de/serialize the transition state to and from a type
+	 * that can be stored in a table.
+	 */
+	if (agg->aggtranstype == INTERNALOID &&
+			transoutname == NIL && combineinname == NIL)
+		elog(ERROR, "combineinfunc and transoutfunc are required for internal transition type");
+
+	finalfn = agg->aggfinalfn;
+	transfn = agg->aggtransfn;
+	transouttype = agg->aggtranstype;
+	ReleaseSysCache(aggtup);
+
+	combinefn = get_single_func_oid(combinename);
+
+	if (combineinname)
+		combineinfn = get_single_func_oid(combineinname);
+
+	if (transoutname)
+	{
+		HeapTuple t = lookup_single_func(transoutname);
+		Form_pg_proc transout = (Form_pg_proc) GETSTRUCT(t);
+
+		if (ArrayGetNItems(ARR_NDIM(&transout->proargtypes), ARR_DIMS(&transout->proargtypes)) != 1)
+			elog(ERROR, "transoutfunc must take exactly one argument");
+
+		transouttype = transout->prorettype;
+		transoutfn = HeapTupleGetOid(t);
+		ReleaseSysCache(t);
+
+		/*
+		 * The transout function's input type must be the same
+		 * as the transition function's return type.
+		 */
+		if (transout->proargtypes.values[0] != agg->aggtranstype)
+			elog(ERROR, "transoutfunc %s must have input type %u", NameListToString(transoutname), agg->aggtranstype);
+	}
+
+	Assert(transouttype != INTERNALOID);
+
+	combinetup = SearchSysCache2(PIPELINECOMBINETRANSFNOID,
+			ObjectIdGetDatum(agg->aggfinalfn), ObjectIdGetDatum(agg->aggtransfn));
+
+	if (HeapTupleIsValid(combinetup))
+	{
+		elog(ERROR, "combine function %u already exists for aggregate with final function %u and transition function %u",
+				((Form_pipeline_combine) combinetup)->combinefn, agg->aggfinalfn, agg->aggtransfn);
+	}
+
+	/* we now have the information we need to create an entry in pipeline_combine */
+	pipeline_combine = heap_open(PipelineCombineRelationId, RowExclusiveLock);
+
+	MemSet(nulls, false, sizeof(nulls));
+
+	values[Anum_pipeline_combine_aggfinalfn - 1] = ObjectIdGetDatum(finalfn);
+	values[Anum_pipeline_combine_transfn - 1] = ObjectIdGetDatum(transfn);
+	values[Anum_pipeline_combine_transoutfn - 1] = ObjectIdGetDatum(transoutfn);
+	values[Anum_pipeline_combine_combineinfn - 1] = ObjectIdGetDatum(combineinfn);
+	values[Anum_pipeline_combine_combinefn - 1] = ObjectIdGetDatum(combinefn);
+	values[Anum_pipeline_combine_transouttype - 1] = ObjectIdGetDatum(transouttype);
+
+	combinetup = heap_form_tuple(pipeline_combine->rd_att, values, nulls);
+	result = simple_heap_insert(pipeline_combine, combinetup);
+	CatalogUpdateIndexes(pipeline_combine, combinetup);
+
+	heap_freetuple(combinetup);
+	heap_close(pipeline_combine, NoLock);
+
+	/*
+	 * Record a dependency between the combine function and its corresponding aggregate
+	 */
+	child.classId = ProcedureRelationId;
+	child.objectId = aggoid;
+	child.objectSubId = 0;
+
+	parent.classId = ProcedureRelationId;
+	parent.objectId = combinefn;
+	parent.objectSubId = 0;
+
+	recordDependencyOn(&child, &parent, DEPENDENCY_NORMAL);
+
+	/*
+	 * Do the same for the transout and combinein functions if they exist
+	 */
+	if (OidIsValid(combineinfn))
+	{
+		child.classId = ProcedureRelationId;
+		child.objectId = aggoid;
+		child.objectSubId = 0;
+
+		parent.classId = ProcedureRelationId;
+		parent.objectId = combineinfn;
+		parent.objectSubId = 0;
+
+		recordDependencyOn(&child, &parent, DEPENDENCY_NORMAL);
+	}
+	if (OidIsValid(transoutfn))
+	{
+		child.classId = ProcedureRelationId;
+		child.objectId = aggoid;
+		child.objectSubId = 0;
+
+		parent.classId = ProcedureRelationId;
+		parent.objectId = transoutfn;
+		parent.objectSubId = 0;
+
+		recordDependencyOn(&child, &parent, DEPENDENCY_NORMAL);
+	}
 
 	return result;
 }
