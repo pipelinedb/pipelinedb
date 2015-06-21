f1ccb2026ecd97a5dfa6dfd1fcd5dbe6d9286c4a
Usman Masood <usmanm@fastmail.fm>
2015-06-21 10:25:29 -0700

    Don't allow dropping group lookup index

--- src/test/regress/sql/hash_group.sql
+++ src/test/regress/sql/hash_group.sql
@@ -1,4 +1,3 @@
-
 SELECT hash_group(0::int, 0::int, 0::int, 0::int);
 SELECT hash_group('0'::text, '2015-02-01'::timestamp, 1.2);
 SELECT hash_group(null::text);
@@ -12,3 +11,15 @@ SELECT hash_group(null::int2, null::int2);
 SELECT date_trunc('second', '2015-01-01'::timestamp) + i * interval '1 second' AS ts, ls_hash_group(date_trunc('second', '2015-01-01'::timestamp) + i * interval '1 second', 0::int4) AS key
 FROM generate_series(1, 100) AS i ORDER BY key;
 
+-- Ensure that hash index is created and cannot be dropped
+CREATE CONTINUOUS VIEW hash_group AS SELECT x::int, COUNT(*) FROM hash_group_stream GROUP BY x;
+CREATE CONTINUOUS VIEW ls_hash_group AS SELECT x::int, y::timestamptz, COUNT(*) FROM hash_group_stream GROUP BY x, y;
+
+\d+ hash_group_mrel0;
+\d+ ls_hash_group_mrel0;
+
+DROP INDEX hash_group_mrel0_expr_idx;
+DROP INDEX ls_hash_group_mrel0_expr_idx;
+
+DROP CONTINUOUS VIEW hash_group;
+DROP CONTINUOUS VIEW ls_hash_group;
