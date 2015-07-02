9a3f41a65a4d3bd531426313627a8432c02fe850
Usman Masood <usmanm@fastmail.fm>
2015-07-02 08:54:54 -0700

    downgrade AX to X

--- src/backend/commands/pipelinecmds.c
+++ src/backend/commands/pipelinecmds.c
@@ -466,7 +466,7 @@ ExecCreateContViewStmt(CreateContViewStmt *stmt, const char *querystring)
 	saveAllowSystemTableMods = allowSystemTableMods;
 	allowSystemTableMods = true;
 
-	pipeline_query = heap_open(PipelineQueryRelationId, AccessExclusiveLock);
+	pipeline_query = heap_open(PipelineQueryRelationId, ExclusiveLock);
 
 	CreateInferredStreams((SelectStmt *) stmt->query);
 	ValidateContQuery(stmt->into->rel, stmt->query, querystring);
