b4ba46bbd841c8d6e252e30beea8e994180e6d15
Jason McSweeney <loadzero.dev@gmail.com>
2015-08-05 17:14:03 -0700

    add missing files

---
+++ src/bin/psql/adhoc_util.c
@@ -0,0 +1,24 @@
+#include "adhoc_util.h"
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ncurses.h>
+
+void
+fatal_error(const char *file, unsigned line, const char *fmt, ...)
+{
+    va_list ap;
+
+	/* clean up after ncurses to make error readable */
+	endwin();
+
+    fprintf(stderr, "padhoc: %s:%d: error: ", file, line);
+
+    va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap);
+    va_end(ap);
+
+    fprintf(stderr, "\n");
+
+    exit(1);
+}
---
+++ src/bin/psql/adhoc_util.h
@@ -0,0 +1,39 @@
+#ifndef ADHOC_UTIL_H
+#define ADHOC_UTIL_H
+
+#include <unistd.h>
+#include <ncurses.h>
+
+void fatal_error(const char *file, unsigned line, const char *fmt, ...)
+	__attribute__ ((format (printf, 3, 4)));
+
+#define FATAL_ERROR(...) { fatal_error(__FILE__, __LINE__, __VA_ARGS__); }
+
+/*
+ * The adhoc client requires its own assert because it needs to clean up
+ * the ncurses window before printing out any errors.
+ */
+
+#ifndef USE_ASSERT_CHECKING
+#define TermAssert(x) 
+#else
+#define TermAssert(x) { if (!(x)) { endwin(); Assert((x)); } }
+#endif
+
+static inline size_t
+spaces(const char *s)
+{
+	size_t cnt = 0;
+
+	while (*s != '\0')
+	{
+		if (*s == ' ')
+			cnt++;
+
+		s++;
+	}
+
+	return cnt;
+}
+
+#endif
