89ff84d4c007364f77afed0625ebb86537bbfe77
Usman Masood <usmanm@fastmail.fm>
2015-08-17 12:02:46 -0700

    :@

--- src/test/py/test_error_backoff.py
+++ src/test/py/test_error_backoff.py
@@ -1,29 +1,38 @@
 from base import pipeline, clean_db
+import math
 import time
 
 
 def test_backoff(pipeline, clean_db):
-  throttle_time = int(pipeline.execute('SHOW continuous_query_error_throttle').first()['continuous_query_error_throttle'].rstrip('s'))
+  throttle_time = int(pipeline.execute('SHOW continuous_query_error_throttle')
+                      .first()['continuous_query_error_throttle'].rstrip('s'))
   
   def insert_events():
     n = 0
+
     # These should succeed
     for _ in xrange(100):
       pipeline.insert('error_stream', ('x', ), [(10, )])
     n += 100
+
     # Insert erroneous input till we're throttled to the max limit
     start = time.time()
+    p = int(math.floor(math.log(throttle_time * 1000, 2)))
+    total = -1 * (1 - math.pow(2, p + 1)) / 1000
+
     while True:
       # These will fail
       for _ in xrange(100):
         pipeline.insert('error_stream', ('x', ), [(0, )])
       n += 100
-      if time.time() - start >= throttle_time * 2:
+      if time.time() - start >= total:
         break
+
     # These should get throttled
     for _ in xrange(100):
       pipeline.insert('error_stream', ('x', ), [(10, )])
     n += 100
+
     time.sleep(throttle_time)
     # These should work
     for _ in xrange(100):
@@ -41,4 +50,3 @@ def test_backoff(pipeline, clean_db):
   n = insert_events()
   assert pipeline.execute('SELECT count(*) FROM error_cv').first()['count'] == 400
   assert pipeline.execute('SELECT count(*) FROM no_error_cv').first()['count'] == n
-
