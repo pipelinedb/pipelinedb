992bfac7d67762ef87c0272f40a874593ad1c9b3
Usman Masood <usmanm@fastmail.fm>
2015-08-16 18:48:48 -0700

    no need for aggs

--- src/test/py/test_error_backoff.py
+++ src/test/py/test_error_backoff.py
@@ -4,54 +4,7 @@ import time
 
 def test_backoff(pipeline, clean_db):
   throttle_time = int(pipeline.execute('SHOW continuous_query_error_throttle').first()['continuous_query_error_throttle'].rstrip('s'))
-
-  pipeline.execute("""
-CREATE OR REPLACE FUNCTION error_sfunc (
-  state int,
-  i     int
-) RETURNS int AS 
-$$
-BEGIN
-  IF i = 1 THEN
-    i := i / 0;
-  END IF;
-
-  IF state IS NULL THEN
-    state := 0;
-  END IF;
-
-  RETURN state + 1;
-END;
-$$
-LANGUAGE 'plpgsql' IMMUTABLE;
-  """)
-
-  pipeline.execute("""
-CREATE OR REPLACE FUNCTION error_combinefunc (
-  state int,
-  i     int
-) RETURNS int AS 
-$$
-BEGIN
-  IF state IS NULL THEN
-    state := 0;
-  END IF;
-
-  RETURN state + i;
-END;
-$$
-LANGUAGE 'plpgsql' IMMUTABLE;
-  """)
-
   
-  pipeline.execute("""
-CREATE AGGREGATE error_agg (int) (
-  stype = int,
-  sfunc = error_sfunc,
-  combinefunc = error_combinefunc
-);
-  """)
-
   def insert_events():
     n = 0
     # These should succeed
@@ -63,7 +16,7 @@ CREATE AGGREGATE error_agg (int) (
     while True:
       # These will fail
       for _ in xrange(100):
-        pipeline.insert('error_stream', ('x', ), [(1, )])
+        pipeline.insert('error_stream', ('x', ), [(0, )])
       n += 100
       if time.time() - start >= throttle_time + 1:
         break
@@ -78,15 +31,14 @@ CREATE AGGREGATE error_agg (int) (
     n += 100
     return n
 
-  pipeline.create_cv('error_cv', 'SELECT error_agg(x::int) FROM error_stream')
+  pipeline.create_cv('error_cv', 'SELECT 100/x::int FROM error_stream')
   insert_events()
 
-  assert pipeline.execute('SELECT error_agg FROM error_cv').first()['error_agg'] == 200
+  assert pipeline.execute('SELECT count(*) FROM error_cv').first()['count'] == 200
 
-  pipeline.create_cv('no_error_cv', 'SELECT count(*) FROM error_stream')
+  pipeline.create_cv('no_error_cv', 'SELECT x::int FROM error_stream')
 
   n = insert_events()
-  assert pipeline.execute('SELECT error_agg FROM error_cv').first()['error_agg'] == 400
-  assert pipeline.execute('SELECT count FROM no_error_cv').first()['count'] == n
-  
-  pipeline.execute('DROP FUNCTION error_sfunc (int, int) CASCADE')
+  assert pipeline.execute('SELECT count(*) FROM error_cv').first()['count'] == 400
+  assert pipeline.execute('SELECT count(*) FROM no_error_cv').first()['count'] == n
+
