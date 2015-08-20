a1aa776db44dfa5654d327516c0d834ea55139be
Usman Masood <usmanm@fastmail.fm>
2015-08-20 09:38:42 -0700

    Change port for Docker pkg

--- pkg/docker/scripts/install_pipeline.sh
+++ pkg/docker/scripts/install_pipeline.sh
@@ -7,7 +7,7 @@ apt-get -f install
 apt-get install -y wget nano
 
 # install dependencies
-apt-get install -y libxml2 libxml2-dev libcurl3
+apt-get install -y libxml2 libxml2-dev
 
 wget -q http://www.pipelinedb.com/download/0.8.0/ubuntu14
 
