7f18207f1cb2dc8e0b558228cd31b2b7a4c4a93b
Jason McSweeney <loadzero.dev@gmail.com>
2015-09-30 10:53:42 -0700

    add copyright notice and doco to bsd rbtree

--- src/bin/psql/bsd_rbtree.c
+++ src/bin/psql/bsd_rbtree.c
@@ -5,17 +5,47 @@
 #include <assert.h>
 #include <stdio.h>
 
+/*-
+ * Copyright (c) 2001 The NetBSD Foundation, Inc.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to The NetBSD Foundation
+ * by Matt Thomas <matt@3am-software.com>.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #define likely(x)       __builtin_expect((x),1)
 #define unlikely(x)     __builtin_expect((x),0)
 #define __predict_false(x) __builtin_expect((x),0)
 
 #define RBSTATS 1
 
+#define	BSD_RB_DIR_LEFT	0
+#define	BSD_RB_DIR_RIGHT 1
+
 typedef struct bsd_rb_node {
 	struct bsd_rb_node *bsd_rb_nodes[2];
 
-#define	BSD_RB_DIR_LEFT		0
-#define	BSD_RB_DIR_RIGHT		1
 #define	BSD_RB_DIR_OTHER		1
 #define	bsd_rb_left			bsd_rb_nodes[BSD_RB_DIR_LEFT]
 #define	bsd_rb_right		bsd_rb_nodes[BSD_RB_DIR_RIGHT]
