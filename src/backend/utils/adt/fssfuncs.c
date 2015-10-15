7920e0d8e302b7576b4dcec37f74aa7134035218
Derek Nelson <=>
2015-10-15 14:59:27 -0700

    #1211 Support byref types for FSS topk

--- src/backend/utils/adt/fssfuncs.c
+++ src/backend/utils/adt/fssfuncs.c
@@ -30,30 +30,6 @@
 
 #define DEFAULT_K 5
 
-static FSS *
-fss_fix_ptrs(struct varlena *bytes)
-{
-	char *pos;
-	FSS *fss = (FSS *) bytes;
-
-	/* Fix pointers */
-	pos = (char *) fss;
-	pos += sizeof(FSS);
-	fss->bitmap_counter = (Counter *) pos;
-	pos += sizeof(Counter) * fss->h;
-	fss->monitored_elements = (MonitoredElement *) pos;
-
-	if (FSS_STORES_DATUMS(fss))
-	{
-		pos += sizeof(MonitoredElement) * fss->m;
-		fss->top_k = (Datum *) pos;
-	}
-	else
-		fss->top_k = NULL;
-
-	return fss;
-}
-
 Datum
 fss_print(PG_FUNCTION_ARGS)
 {
@@ -63,7 +39,7 @@ fss_print(PG_FUNCTION_ARGS)
 	if (PG_ARGISNULL(0))
 		PG_RETURN_NULL();
 
-	fss = fss_fix_ptrs(PG_GETARG_VARLENA_P(0));
+	fss = FSSFromBytes(PG_GETARG_VARLENA_P(0));
 
 	initStringInfo(&buf);
 	appendStringInfo(&buf, "{ m = %d, h = %d, count = %ld, size = %ldkB }", fss->m, fss->h, fss->count, FSSSize(fss) / 1024);
@@ -86,16 +62,16 @@ fss_agg_trans(PG_FUNCTION_ARGS)
 
 	if (PG_ARGISNULL(0))
 	{
-		uint16_t k = PG_GETARG_INT64(2);
+		uint16_t k = PG_GETARG_UINT16(2);
 		Oid type = AggGetInitialArgType(fcinfo);
 		TypeCacheEntry *typ = lookup_type_cache(type, 0);
 		fcinfo->flinfo->fn_extra = typ;
 		state = FSSCreate(k, typ);
 	}
 	else
-		state = fss_fix_ptrs(PG_GETARG_VARLENA_P(0));
+		state = FSSFromBytes(PG_GETARG_VARLENA_P(0));
 
-	FSSIncrement(state, incoming);
+	state = FSSIncrement(state, incoming, PG_ARGISNULL(1));
 
 	MemoryContextSwitchTo(old);
 
@@ -118,16 +94,16 @@ fss_agg_weighted_trans(PG_FUNCTION_ARGS)
 
 	if (PG_ARGISNULL(0))
 	{
-		uint16_t k = PG_GETARG_INT64(2);
+		uint16_t k = PG_GETARG_UINT16(2);
 		Oid type = AggGetInitialArgType(fcinfo);
 		TypeCacheEntry *typ = lookup_type_cache(type, 0);
 		fcinfo->flinfo->fn_extra = typ;
 		state = FSSCreate(k, typ);
 	}
 	else
-		state = fss_fix_ptrs(PG_GETARG_VARLENA_P(0));
+		state = FSSFromBytes(PG_GETARG_VARLENA_P(0));
 
-	FSSIncrementWeighted(state, incoming, weight);
+	state = FSSIncrementWeighted(state, incoming, PG_ARGISNULL(1), weight);
 
 	MemoryContextSwitchTo(old);
 
@@ -149,18 +125,26 @@ fss_agg_transp(PG_FUNCTION_ARGS)
 
 	if (PG_ARGISNULL(0))
 	{
-		uint16_t k = PG_GETARG_INT64(2);
-		uint16_t m = PG_GETARG_INT64(3);
-		uint16_t h = PG_GETARG_INT64(4);
+		uint16_t k = PG_GETARG_UINT16(2);
+		uint16_t m = PG_GETARG_UINT16(3);
+		uint16_t h = PG_GETARG_UINT16(4);
 		Oid type = AggGetInitialArgType(fcinfo);
 		TypeCacheEntry *typ = lookup_type_cache(type, 0);
 		fcinfo->flinfo->fn_extra = typ;
+
+		if (k > m || k > USHRT_MAX)
+			elog(ERROR, "maximum value of k exceeded");
+		if (m > USHRT_MAX)
+			elog(LOG, "maximum value of m exceeded");
+		if (h > USHRT_MAX)
+			elog(ERROR, "maximum value of h exceeded");
+
 		state = FSSCreateWithMAndH(k, typ, m, h);
 	}
 	else
-		state = fss_fix_ptrs(PG_GETARG_VARLENA_P(0));
+		state = FSSFromBytes(PG_GETARG_VARLENA_P(0));
 
-	FSSIncrement(state, incoming);
+	state = FSSIncrement(state, incoming, PG_ARGISNULL(1));
 
 	MemoryContextSwitchTo(old);
 
@@ -173,7 +157,7 @@ fss_merge_agg_trans(PG_FUNCTION_ARGS)
 	MemoryContext old;
 	MemoryContext context;
 	FSS *state;
-	FSS *incoming = fss_fix_ptrs(PG_GETARG_VARLENA_P(1));
+	FSS *incoming = FSSFromBytes(PG_GETARG_VARLENA_P(1));
 
 	if (!AggCheckCallContext(fcinfo, &context))
 		elog(ERROR, "fss_merge_agg_trans called in non-aggregate context");
@@ -186,7 +170,7 @@ fss_merge_agg_trans(PG_FUNCTION_ARGS)
 		PG_RETURN_POINTER(state);
 	}
 
-	state = fss_fix_ptrs(PG_GETARG_VARLENA_P(0));
+	state = FSSFromBytes(PG_GETARG_VARLENA_P(0));
 	state = FSSMerge(state, incoming);
 
 	MemoryContextSwitchTo(old);
@@ -200,6 +184,7 @@ fss_topk(PG_FUNCTION_ARGS)
 	FSS *fss;
 	Datum *datums;
 	uint64_t *freqs;
+	bool *null_k;
 	uint16_t found;
 	Tuplestorestate *store;
 	ReturnSetInfo *rsi;
@@ -209,7 +194,7 @@ fss_topk(PG_FUNCTION_ARGS)
 	if (PG_ARGISNULL(0))
 		PG_RETURN_NULL();
 
-	fss = fss_fix_ptrs(PG_GETARG_VARLENA_P(0));
+	fss = FSSFromBytes(PG_GETARG_VARLENA_P(0));
 	desc= CreateTemplateTupleDesc(2, false);
 	TupleDescInitEntry(desc, (AttrNumber) 1, "value", fss->typ.typoid, -1, 0);
 	TupleDescInitEntry(desc, (AttrNumber) 2, "frequency", INT8OID, -1, 0);
@@ -219,7 +204,7 @@ fss_topk(PG_FUNCTION_ARGS)
 	rsi->setDesc = BlessTupleDesc(desc);
 
 	store = tuplestore_begin_heap(false, false, work_mem);
-	datums = FSSTopK(fss, fss->k, &found);
+	datums = FSSTopK(fss, fss->k, &null_k, &found);
 	freqs = FSSTopKCounts(fss, fss->k, &found);
 
 	for (i = 0; i < found; i++)
@@ -230,6 +215,7 @@ fss_topk(PG_FUNCTION_ARGS)
 
 		MemSet(nulls, false, sizeof(nulls));
 
+		nulls[0] = null_k[i];
 		values[0] = datums[i];
 		values[1] = freqs[i];
 
@@ -254,9 +240,9 @@ fss_topk_values(PG_FUNCTION_ARGS)
 	if (PG_ARGISNULL(0))
 		PG_RETURN_NULL();
 
-	fss = fss_fix_ptrs(PG_GETARG_VARLENA_P(0));
+	fss = FSSFromBytes(PG_GETARG_VARLENA_P(0));
 
-	datums = FSSTopK(fss, fss->k, &found);
+	datums = FSSTopK(fss, fss->k, NULL, &found);
 	typ = lookup_type_cache(fss->typ.typoid, 0);
 	arr = construct_array(datums, found, typ->type_id, typ->typlen, typ->typbyval, typ->typalign);
 
@@ -274,7 +260,7 @@ fss_topk_freqs(PG_FUNCTION_ARGS)
 	if (PG_ARGISNULL(0))
 		PG_RETURN_NULL();
 
-	fss = fss_fix_ptrs(PG_GETARG_VARLENA_P(0));
+	fss = FSSFromBytes(PG_GETARG_VARLENA_P(0));
 
 	datums = FSSTopKCounts(fss, fss->k, &found);
 	arr = construct_array(datums, found, INT8OID, 8, true, 'd');
@@ -295,9 +281,9 @@ Datum
 fss_emptyp(PG_FUNCTION_ARGS)
 {
 	Oid typid = PG_GETARG_OID(0);
-	uint16_t k = PG_GETARG_INT64(1);
-	uint16_t m = PG_GETARG_INT64(2);
-	uint16_t h = PG_GETARG_INT64(3);
+	uint16_t k = PG_GETARG_UINT16(1);
+	uint16_t m = PG_GETARG_UINT16(2);
+	uint16_t h = PG_GETARG_UINT16(3);
 	FSS *fss = FSSCreateWithMAndH(k, lookup_type_cache(typid, 0), m, h);
 	PG_RETURN_POINTER(fss);
 }
@@ -312,13 +298,13 @@ fss_increment(PG_FUNCTION_ARGS)
 		fss = FSSCreate(DEFAULT_K, typ);
 	else
 	{
-		fss = fss_fix_ptrs(PG_GETARG_VARLENA_P(0));
+		fss = FSSFromBytes(PG_GETARG_VARLENA_P(0));
 
 		if (fss->typ.typoid != typ->type_id)
 			elog(ERROR, "type mismatch for incoming value");
 	}
 
-	FSSIncrement(fss, PG_GETARG_DATUM(1));
+	fss = FSSIncrement(fss, PG_GETARG_DATUM(1), PG_ARGISNULL(1));
 
 	PG_RETURN_POINTER(fss);
 }
