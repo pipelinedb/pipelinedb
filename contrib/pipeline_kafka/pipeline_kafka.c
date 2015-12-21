b23396e733517802a2631c6c9710b6a153b1c5f4
Jesper Lundgren <jesper.lundgren@vpon.com>
2015-12-21 13:00:00 +0800

    Fix offset error in pipeline_kafka \n check.
    buf.data[buf.len] !=  '\n' is always true because appendBinaryStringInfo null terminates message payload.

--- contrib/pipeline_kafka/pipeline_kafka.c
+++ contrib/pipeline_kafka/pipeline_kafka.c
@@ -691,7 +691,7 @@ kafka_consume_main(Datum arg)
 				if (messages[i]->payload != NULL)
 				{
 					appendBinaryStringInfo(&buf, messages[i]->payload, messages[i]->len);
-					if (buf.data[buf.len] != '\n')
+					if (buf.len > 0 && buf.data[buf.len - 1] != '\n')
 						appendStringInfoChar(&buf, '\n');
 					messages_buffered++;
 				}
