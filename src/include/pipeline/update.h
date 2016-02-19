76672ad0c286e6592aede9ff60dcc864c44f0fd0
Derek Nelson <=>
2016-02-18 16:26:15 -0800

    Add configuration validation check hook

--- src/include/pipeline/update.h
+++ src/include/pipeline/update.h
@@ -18,6 +18,10 @@ extern bool anonymous_update_checks;
 typedef char *(*GetInstallationIdFunc) (void);
 extern GetInstallationIdFunc GetInstallationIdHook;
 
+typedef void (*VerifyConfigurationFunc) (void);
+extern VerifyConfigurationFunc VerifyConfigurationHook;
+
 extern void UpdateCheck(HTAB *all_dbs, bool startup);
+extern int AnonymousPost(char *path, char *payload, char *resp_buf, int resp_len);
 
 #endif
