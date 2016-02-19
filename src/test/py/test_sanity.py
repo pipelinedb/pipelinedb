cc5b54432e8226e7afe7f0d12d10197f57cffdd2
Usman Masood <usmanm@fastmail.fm>
2016-02-19 08:15:09 -0800

    Regression test for concurrent inserts

--- src/test/py/test_sanity.py
+++ src/test/py/test_sanity.py
@@ -1,6 +1,9 @@
 from base import pipeline, clean_db
+
 import getpass
 import psycopg2
+import threading
+import time
 
 
 def test_create_drop_continuous_view(pipeline, clean_db):
@@ -25,6 +28,7 @@ def test_create_drop_continuous_view(pipeline, clean_db):
 
     assert len(names) == 0
 
+
 def test_simple_insert(pipeline, clean_db):
     """
     Verify that we can insert some rows and count some stuff
@@ -41,6 +45,7 @@ def test_simple_insert(pipeline, clean_db):
     for i, row in enumerate(result):
         assert row['key'] == i
         assert row['count'] == 100
+
 
 def test_multiple(pipeline, clean_db):
     """
@@ -61,6 +66,7 @@ def test_multiple(pipeline, clean_db):
     result = list(pipeline.execute('SELECT * FROM cv1'))
     assert len(result) == 1000
 
+
 def test_combine(pipeline, clean_db):
     """
     Verify that partial tuples are combined with on-disk tuples
@@ -69,7 +75,6 @@ def test_combine(pipeline, clean_db):
 
     rows = []
     for n in range(100):
-        values = []
         for m in range(100):
             key = '%d%d' % (n % 10, m)
             rows.append((key, 0))
@@ -82,6 +87,7 @@ def test_combine(pipeline, clean_db):
         total += row['count']
 
     assert total == 10000
+
 
 def test_multiple_stmts(pipeline, clean_db):
   conn = psycopg2.connect('dbname=pipeline user=%s host=localhost port=%s' % (getpass.getuser(), pipeline.port))
@@ -96,6 +102,7 @@ def test_multiple_stmts(pipeline, clean_db):
   assert len(result) == 1
   assert result[0]['count'] == 100
 
+
 def test_uniqueness(pipeline, clean_db):
   pipeline.create_cv('uniqueness', 'SELECT x::int, count(*) FROM stream GROUP BY x')
 
@@ -108,3 +115,32 @@ def test_uniqueness(pipeline, clean_db):
     'SELECT count(DISTINCT x) FROM uniqueness').first()['count']
 
   assert count == distinct_count
+
+
+def test_concurrent_inserts(pipeline, clean_db):
+  pipeline.create_cv('concurrent_inserts', 'SELECT x::int, count(*) FROM stream GROUP BY x')
+
+  num_threads = 4
+  stop = False
+  inserted = [0] * num_threads
+
+  def insert(i):
+    conn = psycopg2.connect('dbname=pipeline user=%s host=localhost port=%s' % (getpass.getuser(), pipeline.port))
+    cur = conn.cursor()
+    while not stop:
+      cur.execute('INSERT INTO stream (x) SELECT x % 100 FROM generate_series(1, 1000) AS x')
+      conn.commit()
+      time.sleep(0.01)
+      inserted[i] += 1000
+    conn.close()
+
+  threads = [threading.Thread(target=insert, args=(i, )) for i in range(num_threads)]
+  map(lambda t: t.start(), threads)
+
+  time.sleep(15)
+
+  stop = True
+  map(lambda t: t.join(), threads)
+
+  total = pipeline.execute('SELECT sum(count) FROM concurrent_inserts').first()['sum']
+  assert total == sum(inserted)
