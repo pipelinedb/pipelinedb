3924d5c3ef4b74859140d245ecee52fa3602a741
Usman Masood <usmanm@fastmail.fm>
2016-03-08 12:42:23 -0800

    Bump Docker package version

--- pkg/docker/scripts/install_pipeline.sh
+++ pkg/docker/scripts/install_pipeline.sh
@@ -9,7 +9,7 @@ apt-get install -y wget nano
 # install dependencies
 apt-get install -y libxml2 libxml2-dev
 
-wget -q http://www.pipelinedb.com/download/0.8.5/ubuntu14
+wget -q http://www.pipelinedb.com/download/0.8.6/ubuntu14
 
 dpkg --install ubuntu14
 
