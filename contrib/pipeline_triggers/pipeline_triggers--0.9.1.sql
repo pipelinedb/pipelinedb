a020f8998a0402b2b9ecaaa125b838279703a4d3
Jason McSweeney <loadzero.dev@gmail.com>
2016-03-28 15:37:15 -0700

    porting wip. manual integration test working

--- contrib/pipeline_triggers/pipeline_triggers--0.9.1.sql
+++ contrib/pipeline_triggers/pipeline_triggers--0.9.1.sql
@@ -1,2 +1,10 @@
 -- complain if script is sourced in psql, rather than via CREATE EXTENSION
 \echo Use "CREATE EXTENSION pipeline_triggers" to load this file. \quit
+
+CREATE SCHEMA pipeline_triggers;
+
+-- Continuous triggers
+CREATE FUNCTION pipeline_triggers.send_alert_new_row()
+RETURNS trigger
+AS '$libdir/pipeline_triggers', 'send_alert_new_row'
+LANGUAGE C IMMUTABLE;
