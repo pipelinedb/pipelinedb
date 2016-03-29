c5036a6f7a07d13c3c40aff6e1e142107315cd7a
Jason McSweeney <loadzero.dev@gmail.com>
2016-03-29 13:29:55 -0700

    make alert client work with core.

--- src/bin/recv_alerts/options.h
+++ src/bin/recv_alerts/options.h
@@ -193,107 +193,6 @@ get_user_name()
 }
 
 /*
- * read_first_page
- *
- * Read first 4096 bytes of fd and append into string info
- */
-static void
-read_first_page(StringInfo info, int fd)
-{
-	char buf[4096];
-	int nbytes = read(fd, buf, sizeof(buf));
-
-	if (nbytes > 0)
-		appendStringInfo(info, buf, nbytes);
-}
-
-/*
- * append_arg_val
- *
- * Append an argument and a value to supplied command args
- */
-static void
-append_arg_val(CommandArgs *args, const char *flag, const char *val)
-{
-	int index = args->n;
-
-	args->n += 2;
-	args->data = realloc(args->data, (args->n+1) * sizeof(char *));
-
-	Assert(args->data);
-
-	args->data[index] = strdup(flag);
-	args->data[index+1] = strdup(val);
-	args->data[args->n] = NULL;
-}
-
-/*
- * append_arg
- *
- * Append an argument to supplied command args
- */
-static void
-append_arg(CommandArgs *args, const char *flag)
-{
-	int index = args->n;
-
-	args->n += 1;
-	args->data = realloc(args->data, (args->n+1) * sizeof(char *));
-
-	args->data[index] = strdup(flag);
-	args->data[args->n] = NULL;
-}
-
-/*
- * make_pipeline_args
- *
- * Builds the command line args to execve the pipeline binary with.
- */
-static CommandArgs *
-make_pipeline_args(Args *args)
-{
-	char *copy = 0;
-	const char *view_name = 0;
-	char *saveptr = 0;
-	StringInfo qcmd_str;
-
-	CommandArgs *cmd_args = palloc0(sizeof(CommandArgs));
-
-	append_arg(cmd_args, "pipeline");
-
-	if (args->conn.user)
-		append_arg_val(cmd_args, "-U", args->conn.user);
-
-	if (args->conn.host)
-		append_arg_val(cmd_args, "-h", args->conn.host);
-
-	if (args->conn.port)
-		append_arg_val(cmd_args, "-p", args->conn.port);
-
-	if (args->conn.dbname)
-		append_arg_val(cmd_args, "-d", args->conn.dbname);
-
-	if (args->conn.no_password)
-		append_arg(cmd_args, "-w");
-
-	if (args->conn.force_password)
-		append_arg(cmd_args, "-W");
-
-	qcmd_str = makeStringInfo();
-
-	copy = strdup(args->subs);
-	view_name = strtok_r(copy, ".", &saveptr);
-
-	if (!view_name)
-		view_name = "null";
-
-	appendStringInfo(qcmd_str, "copy (" QUERY_STR ") to stdout;", view_name);
-	append_arg_val(cmd_args, "-c", qcmd_str->data);
-
-	return cmd_args;
-}
-
-/*
  * parse_ipc_desc
  *
  * Parse strings like tcp:host:port into an IPCDesc structure
@@ -334,71 +233,12 @@ parse_ipc_desc(IPCDesc *desc, const char* s)
 static void
 auto_configure(Args *args)
 {
-	CommandArgs *cmd_args = make_pipeline_args(args);
+	char ipc_str[256];
 
-	int stdout_pipe_fd[2];
-	int stderr_pipe_fd[2];
-
-	pid_t pid;
-
-	if (pipe(stdout_pipe_fd) == -1)
-		die("pipe error: %m");
-
-	if (pipe(stderr_pipe_fd) == -1)
-		die("pipe error: %m");
-
-	if ((pid = fork()) == -1)
-		die("fork error: %m");
-
-	if (pid == 0)
-	{
-		dup2(stdout_pipe_fd[1], STDOUT_FILENO);
-		dup2(stderr_pipe_fd[1], STDERR_FILENO);
-
-		close(stdout_pipe_fd[0]);
-		close(stdout_pipe_fd[1]);
-
-		close(stderr_pipe_fd[0]);
-		close(stderr_pipe_fd[1]);
-
-		execvp("pipeline", cmd_args->data);
-
-		fprintf(stderr, "could not execute pipeline binary: %m\n");
-		exit(1);
-	}
-	else
-	{
-		int status = 0;
-
-		StringInfo out_lines = makeStringInfo();
-		StringInfo err_lines = makeStringInfo();
-
-		close(stdout_pipe_fd[1]);
-		close(stderr_pipe_fd[1]);
-
-		read_first_page(out_lines, stdout_pipe_fd[0]);
-		read_first_page(err_lines, stderr_pipe_fd[0]);
-
-		wait(&status);
-
-		if (status != 0 || err_lines->len != 0)
-			die_with_help("could not connect to database %s :\n %s",
-						  args->conn.dbname ? args->conn.dbname : "",
-						  err_lines->len ? err_lines->data : "");
-
-		{
-			char *line = 0;
-			char *line_ptr = out_lines->data;
-			char *saveptr = 0;
-
-			while ((args->num_nodes < MAX_NODES) && (line = strtok_r(line_ptr, "\n", &saveptr)))
-			{
-				parse_ipc_desc(&args->nodes[args->num_nodes], line);
-				args->num_nodes++;
-				line_ptr = NULL;
-			}
-		}
-	}
+	snprintf(ipc_str, 256, "tcp:%s:%d", args->conn.host,
+			atoi(args->conn.port) + 2000);
+	args->num_nodes = 1;
+	parse_ipc_desc(&args->nodes[0], ipc_str);
 }
 
 /*
