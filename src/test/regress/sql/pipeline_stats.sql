7f5eb2ad9bd30721971219bd96da92bc0819bfff
Usman Masood <usmanm@fastmail.fm>
2016-04-06 00:29:07 -0700

    rm

--- src/test/regress/sql/pipeline_stats.sql
+++ src/test/regress/sql/pipeline_stats.sql
@@ -29,7 +29,6 @@ SELECT output_rows, errors, cv_create, cv_drop FROM pipeline_stats WHERE type = 
 SELECT input_rows, input_bytes, errors FROM pipeline_stats WHERE type = 'worker';
 
 select pipeline_trigger_debug('die'); select pg_sleep(2);
-select pg_drop_replication_slot((select slot_name from pg_replication_slots order by slot_name desc limit 1));
 
 \c regression
 DROP DATABASE pipeline_stats_db;
