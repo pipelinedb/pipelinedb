8f1bf4b588811f11a8a91d93be2ecd8552cff94d
Usman Masood <usmanm@fastmail.fm>
2016-08-02 09:05:06 -0700

    Revert SIGTERM send

--- src/include/pipeline/cont_scheduler.h
+++ src/include/pipeline/cont_scheduler.h
@@ -114,6 +114,4 @@ extern void SignalContQuerySchedulerRefreshDBList(void);
 
 extern ContQueryDatabaseMetadata *GetContQueryDatabaseMetadata(Oid db_oid);
 
-extern void TerminateContQueryProcesses(void);
-
 #endif   /* CONT_SCHEDULER_H */
