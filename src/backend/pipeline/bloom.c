f4e8a8bc762535bc293eb5ad49169d76280e75cd
André Laszlo <andre@laszlo.nu>
2017-05-18 14:35:46 +0200

    Fix broken url to "Building a Better Bloom Filter"

--- src/backend/pipeline/bloom.c
+++ src/backend/pipeline/bloom.c
@@ -27,7 +27,7 @@
 /*
  * Murmur is faster than an SHA-based approach and provides as-good collision
  * resistance.  The combinatorial generation approach described in
- * http://www.eecs.harvard.edu/~kirsch/pubs/bbbf/esa06.pdf
+ * https://www.eecs.harvard.edu/~michaelm/postscripts/tr-02-05.pdf
  * does prove to work in actual tests, and is obviously faster
  * than performing multiple iterations of Murmur.
  */
