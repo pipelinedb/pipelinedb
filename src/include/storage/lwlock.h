841c6cbf759704fb79b8f86d20d03e75506fe770
Derek Nelson <=>
2017-08-14 13:15:17 -0700

    Remove unused LW locks

--- src/include/storage/lwlock.h
+++ src/include/storage/lwlock.h
@@ -137,9 +137,7 @@ extern PGDLLIMPORT LWLockPadded *MainLWLockArray;
 #define CommitTsLock				(&MainLWLockArray[39].lock)
 #define ReplicationOriginLock		(&MainLWLockArray[40].lock)
 #define MultiXactTruncationLock		(&MainLWLockArray[41].lock)
-#define ContQuerySchedulerLock		(&MainLWLockArray[42].lock)
-#define IPCMessageBrokerIndexLock	(&MainLWLockArray[43].lock)
-#define NUM_INDIVIDUAL_LWLOCKS		44
+#define NUM_INDIVIDUAL_LWLOCKS		42
 
 /*
  * It's a bit odd to declare NUM_BUFFER_PARTITIONS and NUM_LOCK_PARTITIONS
