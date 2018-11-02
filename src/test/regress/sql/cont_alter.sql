5efeb90e2b2e42eef3b51703dc08b87db3f4a8f0
Derek Nelson <=>
2018-11-02 12:25:19 -0700

    Make tests compatible

--- src/test/regress/sql/cont_alter.sql
+++ src/test/regress/sql/cont_alter.sql
@@ -5,6 +5,5 @@ CREATE VIEW test_cont_alter AS SELECT x::int FROM cont_alter_stream;
 ALTER VIEW test_cont_alter RENAME TO lolcat;
 ALTER TABLE test_cont_alter_mrel RENAME TO lolcat;
 ALTER VIEW test_cont_alter ALTER COLUMN x SET DEFAULT 10;
-ALTER TABLE test_cont_alter_mrel DROP COLUMN x;
 
 DROP FOREIGN TABLE cont_alter_stream CASCADE;
